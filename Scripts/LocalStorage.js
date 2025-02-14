ae71c77a89c33a769e47018eeff3743be1a29f33
ChristmasLover12345 <madnes0205@gmail.com>
2025-02-14 10:39:17 -0800

    Added frontend and started on logic

---
+++ Scripts/LocalStorage.js
@@ -0,0 +1,9 @@
+const task = {
+    name: "Task Name",
+    description: "Task Description",
+    status: "Incomplete", 
+    priorityStatus: "High", 
+    dueDate: "2023-12-31" 
+};
+
+
---
+++ Scripts/app.js
@@ -0,0 +1,12 @@
+
+    const incompleteTasks = document.getElementById('incomplete-tasks');
+    const inProgressTasks = document.getElementById('in-progress-tasks');
+    const completeTasks = document.getElementById('complete-tasks');
+
+    const taskName = document.getElementById('task-name');
+    const taskDesc = document.getElementById('task-desc');
+    const dueDate = document.getElementById('due-date');
+    const priority = document.getElementById('priority');
+    const progress = document.getElementById('progress');
+
+    const addTaskBtn = document.getElementById('addTaskBtn');
